322ad96e16f84b2d25b84a2b25b4f0847811494a
Mohammad Osman Faruq <muhammadosmanfaruk@gmail.com>
2021-12-24 12:54:27 +0600

    done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant/body.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,9 +13,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
+      appBar: buildAppBar(),
+      body: Body(),
     );
+  }
+  AppBar buildAppBar(){
+    return AppBar(
+        elevation: 0,
+        leading: IconButton(
+          onPressed: (){},
+            icon: SvgPicture.asset("assets/icons/menu.svg"),
+          ),
+      );
   }
 }
